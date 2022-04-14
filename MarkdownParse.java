c91243f58388e96511c820c388b0e2c82567bb4b
ajhahn <ajhahn@ucsd.edu>
2022-04-14 15:30:54 -0700

    Second-file-test.mc Bug Fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,13 +11,26 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+       
         for (currentIndex = 0; currentIndex < markdown.length(); currentIndex++ ) {
+            boolean sign = false;
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (markdown.contains("!")) {
+                sign = true;
+            }
+            if (sign = false) {
+            toReturn.add(markdown.substring(openParen + 1, closeParen)); 
+            }
+            else {
+                continue;
+            }
             currentIndex = closeParen + 1;
+            sign = false;
+            
+
         }
 
         return toReturn;
