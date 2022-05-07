3123ba28514d7988b8dc8f83256e6958458556d2
ajhahn <ajhahn@ucsd.edu>
2022-05-06 20:16:11 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,9 @@
 import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.*;
 
 public class MarkdownParseTest {
